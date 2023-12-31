1aac8c5851f8d71a4d4d13b57e7e037524a1c091
Shreyash <shreyashp47@gmail.com>
2023-12-31 19:58:07 +0530

    Fixing Image load in cells

--- iMoodi/Controllers/ChatViewController.swift
+++ iMoodi/Controllers/ChatViewController.swift
@@ -8,22 +8,78 @@
 import UIKit
 
 class ChatViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var apiManager = ApiManager()
+
+    var feeds : [PhotoListData] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        apiManager.delegate = self
+        //apiManager.fetchPhotoList()
+        
+        feeds.append(PhotoListData(id: "131", description: "sdcdj  sdcdjsdcd sdcdjsdcd sdcdjsdcd sdcd  sdcdjsdcd  sdcdjsdcd  sdcdjsdcd jsdcdj", urls: nil, user: nil))
+        feeds.append(PhotoListData(id: "131", description: "sdcdj  sdcdjsdcd sdcdjsdcd sdcdjsdcd sdcd  sdcdjsdcd  sdcdjsdcd  sdcdjsdcd jsdcdj", urls: nil, user: nil))
+        feeds.append(PhotoListData(id: "131", description: "sdcdj  sdcdjsdcd sdcdjsdcd sdcdjsdcd sdcd  sdcdjsdcd  sdcdjsdcd  sdcdjsdcd jsdcdj", urls: nil, user: nil))
+        feeds.append(PhotoListData(id: "131", description: "sdcdj  sdcdjsdcd sdcdjsdcd sdcdjsdcd sdcd  sdcdjsdcd  sdcdjsdcd  sdcdjsdcd jsdcdj", urls: nil, user: nil))
+        feeds.append(PhotoListData(id: "131", description: "sdcdj  sdcdjsdcd sdcdjsdcd sdcdjsdcd sdcd  sdcdjsdcd  sdcdjsdcd  sdcdjsdcd jsdcdj", urls: nil, user: nil))
+        feeds.append(PhotoListData(id: "1e31", description: "sdcdj", urls: nil, user: nil))
+        
+        tableView.dataSource = self
+    
+        
+        tableView.register(UINib(nibName: Constants.messageSellNibName, bundle: nil), forCellReuseIdentifier: Constants.cellIdentifier)
+        
+    
+    }
+
+
+}
+
+extension ChatViewController: APIManagerDelegate {
+    
+    func didUpdateAPIList(_ apimanager: ApiManager, photoListData: [PhotoListData]) {
+        print("didUpdateAPIList")
+        if !photoListData.isEmpty {
+            print(photoListData[0].id!)
+            for feed in photoListData {
+                feeds.append(feed)
+            }
+            DispatchQueue.main.async{
+                self.tableView.reloadData()
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  
+    
+    func didFailWithError(error: Error) {
+        print("didFailWithError")
     }
-    */
+    
+    
+    
+}
 
+extension ChatViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("count = \(feeds.count)")
+       return feeds.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        as! MessageCell
+        //cell.feedUserName?.text = feeds[indexPath.row].user?.username
+        //cell.label?.text = feeds[indexPath.row].description
+        
+        
+        return cell
+    }
+    
+    
 }
---
+++ iMoodi/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Shreyash Pattewar on 27/12/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageview: UIImageView!
+    //@IBOutlet weak var rightImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+
+        
+        
+    }
+    
+}
